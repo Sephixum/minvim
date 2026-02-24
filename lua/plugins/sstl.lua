ba45a787efd4eb44093d0b8abf18a2365671118e
Sephixum <emotive.5353@gmail.com>
2026-02-25 01:47:24 +0330

    Clean up plugins

--- lua/plugins/sstl.lua
+++ lua/plugins/sstl.lua
@@ -6,12 +6,3 @@ return {
     require("sstl").setup()
   end
 }
-
--- return {
---   "sstl",
---   virtual = true,   -- Tell Lazy this isn't a real git repo
---   config = function()
---     -- Neovim will find this because it's already in the lua/ path
---     require("custom.sstl.lua.sstl").setup()
---   end,
--- }
